c8faeeafdf329497660bbd4573080ea048f978c7
Dominik H <dhitschmann@libe.de>
2018-05-24 01:13:21 +0200

    Version 1.2 Lösche Daten bei installation.

--- src/installed/installed.js
+++ src/installed/installed.js
@@ -1,3 +1,5 @@
 browser.runtime.onInstalled.addListener(function (object) {
-    browser.tabs.create({url: "https://bitbucket.org/domidodo/steam-game-covers-add-on/wiki/Sync"}, null);
+	browser.storage.local.clear();
+	
+    //browser.tabs.create({url: "https://bitbucket.org/domidodo/steam-game-covers-add-on/wiki/Sync"}, null);
 });
